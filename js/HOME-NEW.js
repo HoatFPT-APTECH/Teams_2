e987466d206dba4933ce2b8c79d0c3d392ce5d9d
HoatFPT-APTECH <92987132+HoatFPT-APTECH@users.noreply.github.com>
2021-12-25 11:29:26 +0700

    Update

--- js/HOME-NEW.js
+++ js/HOME-NEW.js
@@ -12,3 +12,20 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides,5000); // Change image every 5 seconds
 }
+function mo(a){
+    var m=document.getElementsByClassName("drink")
+    if(a!=1) {
+        document.getElementById("unnone").style.display = 'none';
+        for (var i = 0; i < m.length; i++) {
+            m[i].style.display = "none"
+        }
+        m[a-2].style.display = "block";
+    }
+    else{
+        for (var i = 0; i < m.length; i++) {
+            m[i].style.display = "none"
+        }
+        document.getElementById("unnone").style.display="block"
+    }
+
+}
